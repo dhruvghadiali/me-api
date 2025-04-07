4e9fd9c9a53f3f4d9c085a8a28d80c136bce54ee
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-04-07 22:51:25 +0530

    Updated routes

--- src/routes/superAdminRouter/index.js
+++ src/routes/superAdminRouter/index.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
 const authRouter = require("@MERoutes/superAdminRouter/authRouter");
-const eductionBoardRouter = require("@MERoutes/superAdminRouter/eductionBoardRouter");
+const stateRouter = require("@MERoutes/superAdminRouter/stateRouter");
 const schoolTypeRouter = require("@MERoutes/superAdminRouter/schoolTypeRouter");
+const eductionBoardRouter = require("@MERoutes/superAdminRouter/eductionBoardRouter");
 
 const router = express.Router();
 
 router.use("/", authRouter);
-router.use("/", eductionBoardRouter);
+router.use("/", stateRouter);
 router.use("/", schoolTypeRouter);
+router.use("/", eductionBoardRouter);
 
 module.exports = router;
---
+++ src/routes/superAdminRouter/stateRouter.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+const { protect } = require("@MEMiddleware/auth");
+const {
+  addState,
+  getStates,
+  updateState,
+  deleteState,
+} = require("@MEControllers/stateController/stateController");
+
+const router = express.Router();
+
+router
+  .route("/states")
+  .get(protect, getStates)
+  .post(protect, addState);
+router
+  .route("/states/:id")
+  .put(protect, updateState)
+  .delete(protect, deleteState);
+
+module.exports = router;
