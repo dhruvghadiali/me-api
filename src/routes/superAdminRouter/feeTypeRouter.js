088630f604a62f0415c0968821b0074db9b4e6bb
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-04-12 02:55:14 +0530

    Updated API route for fee type

---
+++ src/routes/superAdminRouter/feeTypeRouter.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const { protect } = require("@MEMiddleware/auth");
+const {
+  addFeeType,
+  getFeeTypes,
+  updateFeeType,
+  deleteFeeType,
+} = require("@MEControllers/feeTypeController/feeTypeController");
+
+const router = express.Router();
+
+router.route("/fee-types").get(protect, getFeeTypes).post(protect, addFeeType);
+router
+  .route("/fee-types/:id")
+  .put(protect, updateFeeType)
+  .delete(protect, deleteFeeType);
+
+module.exports = router;
--- src/routes/superAdminRouter/index.js
+++ src/routes/superAdminRouter/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const authRouter = require("@MERoutes/superAdminRouter/authRouter");
 const stateRouter = require("@MERoutes/superAdminRouter/stateRouter");
+const feeTypeRouter = require("@MERoutes/superAdminRouter/feeTypeRouter");
 const schoolTypeRouter = require("@MERoutes/superAdminRouter/schoolTypeRouter");
 const eductionBoardRouter = require("@MERoutes/superAdminRouter/eductionBoardRouter");
 const academicGradeRouter = require("@MERoutes/superAdminRouter/academicGradeRouter");
@@ -10,6 +11,7 @@ const router = express.Router();
 
 router.use("/", authRouter);
 router.use("/", stateRouter);
+router.use("/", feeTypeRouter);
 router.use("/", schoolTypeRouter);
 router.use("/", eductionBoardRouter);
 router.use("/", academicGradeRouter);
