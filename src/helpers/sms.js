99088917964e12532502061d6e54f00a9c63867e
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-11-25 21:39:48 +0530

    Updated SMS services

--- src/helpers/sms.js
+++ src/helpers/sms.js
@@ -9,10 +9,11 @@ exports.sendSMSOTP = async (data) => {
 
     await client.messages.create({
       body: `Your OTP code is: ${data.otp}`,
-      from: `${process.env.SEND_SMS_PHONE}`,
+      from: `${process.env.SEND_SMS_PHONE_NUMBER}`,
       to: data.toPhoneNumber,
     });
   } catch (error) {
+    console.error("Error sending SMS OTP:", error);
     return error;
   }
 };
