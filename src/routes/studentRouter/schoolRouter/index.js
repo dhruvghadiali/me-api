1b178029dc69a3758c98a367084d7099ba53bb39
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-12-15 21:03:07 +0530

    Added school router for students

---
+++ src/routes/studentRouter/schoolRouter/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const {
+  getSchool,
+  getSchoolsSummary,
+} = require("@MEControllers/schoolController/schoolController");
+
+const router = express.Router();
+
+/**
+ * @swagger
+ * /schools:
+ *   get:
+ *     summary: Get all schools list
+ *     tags: [Public]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of schools
+ *       400:
+ *         description: Invalid request or validation error
+ *       401:
+ *         description: Unauthorized
+ */
+router.route("/schools").get(getSchoolsSummary);
+router.route("/school/:id").get(getSchool);
+
+module.exports = router;
