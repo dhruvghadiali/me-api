aeca06d6a6aeda0eddc69e6e1a45fa7ed9f68144
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-12-30 21:33:53 +0530

    Updated API routes for student profile

--- src/routes/studentRouter/index.js
+++ src/routes/studentRouter/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const authRouter = require("@MERoutes/studentRouter/authRouter");
 const schoolRouter = require("@MERoutes/studentRouter/schoolRouter");
+const studentProfileRouter = require("@MERoutes/studentRouter/studentProfile/index");
 const addressProfileRouter = require("@MERoutes/studentRouter/addressProfileRouter");
 const admissionApplicationRouter = require("@MERoutes/studentRouter/admissionApplicationRouter");
 
@@ -10,6 +11,7 @@ const router = express.Router();
 router.use("/", authRouter);
 router.use("/", schoolRouter);
 router.use("/", addressProfileRouter);
+router.use("/", studentProfileRouter);
 router.use("/", admissionApplicationRouter);
 
 module.exports = router;
