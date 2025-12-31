43133e8387702865dcbc6982e87ddb108902e8f6
dhruv ghadiali <dhruvghadiali@hotmail.com>
2025-12-31 23:51:32 +0530

    Updated parent profile router

--- src/controllerValidators/parentProfileValidator/addParentProfileValidator.js
+++ src/controllerValidators/parentProfileValidator/addParentProfileValidator.js
@@ -61,7 +61,6 @@ const {
   aadhaarNumberMaxLength,
   parentProfileOccupationBase,
   parentProfileOccupationEmpty,
-  parentProfileOccupationInvalid,
   parentProfileOccupationRequired,
   parentProfileEducationBase,
   parentProfileEducationEmpty,
