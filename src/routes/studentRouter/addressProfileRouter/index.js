5aefae9dd9dee408766124eed9e0f7f313496810
dhruv ghadiali <dhruvghadiali@hotmail.com>
2026-01-02 01:00:53 +0530

    Updated route for update address

--- src/routes/studentRouter/addressProfileRouter/index.js
+++ src/routes/studentRouter/addressProfileRouter/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 
 const addAddressProfileRouter = require("@MERoutes/studentRouter/addressProfileRouter/addAddressProfileRouter");
+const updatedAddressProfileRouter = require("@MERoutes/studentRouter/addressProfileRouter/updateAddressProfileRouter");
 
 const router = express.Router();
 
 router.use("/", addAddressProfileRouter);
+router.use("/", updatedAddressProfileRouter);
 
 module.exports = router;
