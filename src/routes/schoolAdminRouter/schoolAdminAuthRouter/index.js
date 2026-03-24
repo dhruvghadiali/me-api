7c52fd5ce7c578e5f7888168d11c6a1699e5a022
Dhruvkumar Ghadiali <dhruvghadiali@hotmail.com>
2026-03-24 16:55:03 +0530

    Updated school admin authentication router, validation and controller

--- src/routes/schoolAdminRouter/schoolAdminAuthRouter/index.js
+++ src/routes/schoolAdminRouter/schoolAdminAuthRouter/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 
 const schoolAdminSigninRouter = require("@ME/routes/schoolAdminRouter/schoolAdminAuthRouter/schoolAdminSigninRouter");
+const updateSchoolAdminPasswordRouter = require("@MERoutes/schoolAdminRouter/schoolAdminAuthRouter/updateSchoolAdminPasswordRouter");
+const updateSchoolAdminUsernameRouter = require("@MERoutes/schoolAdminRouter/schoolAdminAuthRouter/updateSchoolAdminUsernameRouter");
 
 const router = express.Router();
 
 router.use("/", schoolAdminSigninRouter);
+router.use("/", updateSchoolAdminPasswordRouter);
+router.use("/", updateSchoolAdminUsernameRouter);
 
 module.exports = router;
